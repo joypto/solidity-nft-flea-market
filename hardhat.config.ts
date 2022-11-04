e69502407e8f8bf6df1589e0df7ba352e7b96a30
jopyto <sooie35@gmail.com>
2022-11-05 00:03:13 +0900

    add hardhat config settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,29 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import 'hardhat-abi-exporter'
+import 'hardhat-contract-sizer'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.17',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 1000000
+                    }
+                }
+            }
+        ]
+    },
+    abiExporter: {
+        path: './data/abi',
+        runOnCompile: true,
+        clear: true,
+        flat: false,
+        spacing: 4
+    }
+}
 
-export default config;
+export default config
